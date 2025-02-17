693966903f2e2b574bb969b94bd54fc06eabaafb
1nsanegg <107063052+1nsanegg@users.noreply.github.com>
2025-02-17 22:12:15 +0700

    first commit: create paddle and basic function

---
+++ main.py
@@ -0,0 +1,39 @@
+from turtle import Screen
+from turtle import Turtle
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height= 600)
+screen.title("Pong")
+screen.tracer(0)
+paddle = Turtle()
+paddle.color("white")
+paddle.shape("square")
+
+paddle.shapesize(stretch_len=1, stretch_wid=5)
+
+paddle.penup()
+
+
+paddle.goto(x=350, y = 0)
+
+def go_up():
+    new_y = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), new_y)
+
+def go_down():
+    new_y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), new_y)
+screen.listen()
+screen.onkey(go_up, "Up")
+screen.onkey(go_down, "Down")
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+
+
+
+
+screen.exitonclick()
